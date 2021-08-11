80f07252980a5929174e1031adee6ac13db25404
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:35:52 -0300

    Uri 1072 Intervalo

---
+++ Intervalo2/Intervalo2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Intervalo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int contInt = 0;
+            int contExt = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    contInt = contInt + 1;
+                }
+                else
+                {
+                    contExt = contExt + 1;
+                }
+ 
+            }
+            Console.WriteLine(contInt + " in");
+            Console.WriteLine(contExt + " out");
+
+        }
+    }
+}
